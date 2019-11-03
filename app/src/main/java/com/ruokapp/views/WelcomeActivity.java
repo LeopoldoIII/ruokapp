db3574b2718f45b60bea5e52c251dd5883655a92
David Santiago <davidsghz@gmail.com>
2019-11-03 15:17:48 -0300

    set User params if user has session on WelcomeActivity

--- app/src/main/java/com/ruokapp/views/WelcomeActivity.java
+++ app/src/main/java/com/ruokapp/views/WelcomeActivity.java
@@ -3,11 +3,15 @@ package com.ruokapp.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.ruokapp.R;
+import com.ruokapp.core.db.DBUtils;
+import com.ruokapp.core.db.SQLiteHandler;
 import com.ruokapp.core.session.Session;
+import com.ruokapp.core.user.User;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -25,6 +29,7 @@ public class WelcomeActivity extends AppCompatActivity {
                 try {
                     if(Session.getInstance(getApplicationContext()).getUserSession() != null){
                         intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        setUser();
                     }
                 } catch (Exception e){
                     e.printStackTrace();
@@ -35,7 +40,19 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         }, 500);
 
+    }
 
+    private void setUser(){
+        String[] fields = {DBUtils.ID_USER, DBUtils.USER_NAME, DBUtils.USER_EMAIL};
+        String[] params = {
+                Session.getInstance(this).getUserSession()[0],
+                Session.getInstance(this).getUserSession()[1]};
+        Cursor cursor = SQLiteHandler.searchUserFromLogin(this,fields,params);
+            cursor.moveToFirst();
+            User.getInstanceUser()
+                    .setUser(cursor.getInt(0)
+                            ,cursor.getString(1)
+                            ,cursor.getString(2));
     }
 
     @Override
