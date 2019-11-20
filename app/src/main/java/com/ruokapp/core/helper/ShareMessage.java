c0d51a8b709754366542e4ff9af634ff838049a8
David Santiago <davidsghz@gmail.com>
2019-11-20 00:03:43 -0300

    create ShareMessage class

---
+++ app/src/main/java/com/ruokapp/core/helper/ShareMessage.java
@@ -0,0 +1,10 @@
+package com.ruokapp.core.helper;
+
+public class ShareMessage {
+
+    public static final String SUBJECT = "Could you see it on Ruokapp";
+
+    public static final String TEXT = "Hi!, I want share this recipe with you";
+
+    public static final String SHARE = "Share using";
+}
